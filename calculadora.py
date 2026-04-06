f0b793806a81bd17199e48cb1d3dd2deb7fb3381
rldurann-web <rldurann@icloud.com>
2026-04-07 00:40:02 +0200

    Add calculadora.py for basic calculator functionality

---
+++ calculadora.py
@@ -0,0 +1,11 @@
+numero_1 =float (input ("Primer número = "))
+Operación =input("Operacion (+,-,/,*)= ")
+numero_2 =float (input("Segundo número = "))
+if Operación  == "+":
+ print  (f"{numero_1} + {numero_2} = {numero_1 + numero_2}")
+if Operación == "-":
+  print  (f"{numero_1} - {numero_2} = {numero_1 - numero_2}")
+if Operación == "*":
+    print  (f"{numero_1} * {numero_2} = {numero_1 * numero_2}")
+if Operación == "/":
+    print  (f"{numero_1} / {numero_2} = {numero_1 / numero_2}")
